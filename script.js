467d197e816c6593391d4c124f17dfb2faec7775
earlbalageo-dev <earlbalageo.dev@gmail.com>
2021-07-20 00:40:15 -0700

    contact, footer

--- script.js
+++
@@ -1,8 +0,0 @@
-const copyText = document.querySelector('.email');
-
-copyText.onclick = () => {
-  copyText.select();
-  copyText.setSelectionRange(0, 99999);
-  document.execCommand('copy');
-  alert('Copied the text: ' + copyText.value);
-};
