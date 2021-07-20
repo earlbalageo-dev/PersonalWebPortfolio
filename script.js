e4987915d64ff226c7331c5aa7482c3d253ddd87
earlbalageo-dev <earlbalageo.dev@gmail.com>
2021-07-20 03:01:56 -0700

    dark and light mode

---
+++ script.js
@@ -0,0 +1,36 @@
+const darkButton = document.querySelector('.dark');
+const lightButton = document.querySelector('.light');
+const odd = document.querySelector('body');
+const even = document.querySelectorAll('.even');
+const card = document.querySelectorAll('.card');
+const darkButtonOutline = document.querySelector('.darkRadio');
+
+darkButton.onclick = () => {
+  odd.classList.add('darkOdd');
+  darkButtonOutline.classList.remove('btn-outline-dark');
+  darkButtonOutline.classList.add('btn-outline-light');
+  for (let item of even) {
+    item.classList.remove('lightEven');
+    item.classList.add('darkEven');
+  }
+
+  for (let item of card) {
+    item.classList.remove('lightCard');
+    item.classList.add('darkCard');
+  }
+};
+
+lightButton.onclick = () => {
+  odd.classList.remove('darkOdd');
+  darkButtonOutline.classList.remove('btn-outline-ligth');
+  darkButtonOutline.classList.add('btn-outline-dark');
+  for (let item of even) {
+    item.classList.remove('darkEven');
+    item.classList.add('lightEven');
+  }
+
+  for (let item of card) {
+    item.classList.remove('darkCard');
+    item.classList.add('lightCard');
+  }
+};
